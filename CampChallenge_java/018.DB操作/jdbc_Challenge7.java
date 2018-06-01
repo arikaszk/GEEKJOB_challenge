d1e46034d94c247587b5e3482068f4ccc697e48a
Arika Suzuki <>
2018-06-01 11:21:54 +0900

    課題【018.DB操作-7.特定のレコードの更新】提出します

---
+++ CampChallenge_java/018.DB操作/jdbc_Challenge7.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc_challenge;
+import java.sql.*;
+
+
+/**
+ *
+ * @author Arika
+ */
+public class jdbc_Challenge7 {
+    public static void main (String[] args) throws Exception{
+        
+        Connection con = null;
+        
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection
+        ("jdbc:mysql://localhost:8889/Challenge_db", "root", "root");  //rootのパスワードに"root"を設定しています
+            System.out.println("サーバーへの接続に成功しました");
+            
+            /*
+                課題 テーブルの作成とinsertで作成したテーブル
+                テーブル名：profiles
+                カラム名 データ型：profilesID int, name varchar(255), tel varchar(255), age int, birthday date
+            
+                現時点で挿入されているレコードは3つ
+            */
+            
+            String upd1 = "UPDATE profiles SET name = ?, tel = ?, age = ?, birthday = ? WHERE profilesID = ?";
+            PreparedStatement st = con.prepareStatement(upd1);
+            st.setString(1, "山下　清");
+            st.setString(2, "03-8888-9999");
+            st.setInt(3, 33);
+            st.setString(4, "1984-01-01");
+            st.setInt(5, 4);
+            
+            int num = st.executeUpdate();
+            
+            
+            String slc = "SELECT * FROM profiles";
+            PreparedStatement pst = con.prepareStatement(slc);
+            
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                System.out.println("ID：" + rs.getString("profilesID"));
+                System.out.println("名前：" + rs.getString("name"));
+                System.out.println("電話：" + rs.getString("tel"));
+                System.out.println("年齢：" + rs.getString("age"));
+                System.out.println("誕生日：" + rs.getString("birthday"));
+                System.out.println("");
+            }
+            
+            con.close();
+            
+        } catch(SQLException e) {
+            
+            System.out.println("エラーが起きました" );
+            throw new Exception();
+            
+        } finally {
+            
+            try {
+                if(con != null){
+                    con.close();
+                }
+            } catch(SQLException e) {
+                System.out.println("エラーが起きました");
+            }
+            
+        }
+    }
+}
